095be6d1de47d3c4bb39023a08d83a70a95ea317
DAshadow-dev <duyan9646@gmail.com>
2025-02-16 10:39:31 +0700

    refactor(route) : Add staff route

--- frontend-mobile/src/screens/HomeScreen.tsx
+++ frontend-mobile/src/screens/HomeScreen.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import * as Routes from '@utils/Routes';
-import { navigate, useNavigationRoot } from '@components/navigate/RootNavigation';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import * as Routes from "@utils/Routes";
+import {
+  navigate,
+  useNavigationRoot,
+} from "@components/navigate/RootNavigation";
 
 const HomeScreen = () => {
-    const navigation = useNavigationRoot();
+  const navigation = useNavigationRoot();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home Screen</Text>
-      <Button title="Admin Dashboard" onPress={() => navigation.navigate(Routes.AdminDashboardScreen)}/>
-      <Button title="Go to InvoiceListScreen" onPress={() => navigation.navigate(Routes.InvoiceListScreen)} />
-      <Button title="Go to ProductListScreen" onPress={() => navigation.navigate(Routes.ProductListScreen)} /> 
+      <Button
+        title="Admin Dashboard"
+        onPress={() => navigation.navigate(Routes.AdminDashboardScreen)}
+      />
+      <Button
+        title="Go to InvoiceListScreen"
+        onPress={() => navigation.navigate(Routes.InvoiceListScreen)}
+      />
+      <Button
+        title="Go to ProductListScreen"
+        onPress={() => navigation.navigate(Routes.ProductListScreen)}
+      />
+      <Button
+        title="Go to StaffListScreen"
+        onPress={() => navigation.navigate(Routes.StaffListScreen)}
+      />
     </View>
   );
-}
+};
 export default HomeScreen;
